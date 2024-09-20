be5650fb8299db80789efa1c36f00b2d4aeb9074
Rupam Das <somadaslove9876@gmail.com>
2024-09-20 10:48:47 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ const projectRoutes = require('./modules/projectRoutes');
 
 // middleweres
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	origin: "https://portfolio-admin.vercel.app"
+}));
 app.use('/api', projectRoutes)
 
 // mongoose connection
